16fe706cbf06e25e2ce024554f0c5450216fdb1a
Prraneth-Kumar <prranethkumar22@gmail.com>
2022-10-08 14:07:25 +0530

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Prraneth Kumar A R on 08/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
